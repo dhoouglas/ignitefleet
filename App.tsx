d868de4f67fe408fefd47fc4215d1beb986d64e2
dhoouglas <dhoouglas@hotmail.com>
2025-02-04 12:35:23 -0300

    feat: created components Loading and Button, and implemented screen Sign In

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import { ThemeProvider } from "styled-components/native";
-import { SignIn } from "./src/screens/Signin";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import theme from "./src/theme";
 
+import { SignIn } from "./src/screens/Signin";
+import { Loading } from "./src/components/Loading";
+import { StatusBar } from "react-native";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
-    // <ThemeProvider theme={theme}>
-    <SignIn />
-    // </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </ThemeProvider>
   );
 }
--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -6,14 +6,3 @@ declare module "styled-components" {
 
   export interface DefaultTheme extends ThemeType {}
 }
-
-declare module "styled-components/native" {
-  export interface DefaultTheme {
-    borderRadius: string;
-
-    colors: {
-      main: string;
-      secondary: string;
-    };
-  }
-}
